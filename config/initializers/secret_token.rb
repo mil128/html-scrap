ad33c014e3a8374942a774d0272167c17756486f
Puneet Singh <puneet@milaap.org>
2012-02-17 15:16:26 +0530

    HTML Scrap Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HtmlScrap::Application.config.secret_token = '36ee7fbcab26931f9ffca6383ac8bd572444a592a16ced0f4756664b017c209d5e23c03e486e8de206be7d721fca4b834c00af70e8d37d0d337999a0992a9a24'
