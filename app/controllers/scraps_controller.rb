7fe028ffe50fc7e3b3f22cfc3c165ab1b0d41a1b
Puneet Singh <puneet@milaap.org>
2012-02-20 15:23:53 +0530

    Database Op + Nifty Layout

--- app/controllers/scraps_controller.rb
+++ app/controllers/scraps_controller.rb
@@ -1,4 +1,7 @@
 class ScrapsController < ApplicationController
+  require 'open-uri'
+  require 'nokogiri'
+  
   def index
     @scraps = Scrap.all
     #    if @scraps == []
@@ -42,7 +45,50 @@ class ScrapsController < ApplicationController
   def upload
   end
   def process_url
+    html_doc = Nokogiri::HTML(open(params[:url_text]))
     
-    redirect_to scraps_path
+    fail=0
+    count=0
+
+    html_doc.xpath("//html/body/table/tr").each do |node|
+
+      arr = Array.new
+
+      node.css("td").each do |row|
+        arr << row.inner_text.strip
+      end
+
+      if(arr[0]=="Order ID")
+        next
+      else
+        scr = Scrap.new
+        scr.order_id = arr[0]
+        scr.order_date = DateTime.parse(arr[1])
+        scr.order_amt = arr[2].to_f
+        scr.order_status = arr[3]
+        scr.payment_mode = arr[4]
+        scr.mil_tx_id = arr[5]
+        scr.order_desc = arr[6]
+        scr.borrower_id = arr[7].to_i
+        scr.borrower_name = arr[8]
+        scr.cust_name = arr[9]
+        scr.cust_email = arr[10]
+        scr.cust_phone = arr[11]
+        scr.cust_addr = arr[12]
+
+       if scr.save
+         count = count+1
+       else
+         fail = fail+1
+       end
+        
+      end
+      
+    end
+
+
+
+    
+    redirect_to scraps_path,:notice => "Records Saved:: #{count}, Records failed:: #{fail}"
   end
 end
